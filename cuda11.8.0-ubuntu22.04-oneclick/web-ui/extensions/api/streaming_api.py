26d933d6ec84c8107ee8edf94cfd30228163a2ec
Srđan Radović <srdjan.radovic99@gmail.com>
2023-10-18 18:53:39 +0200

    Use model directly with FastAPI

--- cuda11.8.0-ubuntu22.04-oneclick/web-ui/extensions/api/streaming_api.py
+++ cuda11.8.0-ubuntu22.04-oneclick/web-ui/extensions/api/streaming_api.py
@@ -1,13 +1,12 @@
 import asyncio
+import functools
 import json
 from threading import Thread
-
-import websockets
+import threading
 
 from extensions.api.util import (
     build_parameters,
-    try_start_cloudflared,
-    with_api_lock
+    try_start_cloudflared
 )
 from modules import shared
 from modules.chat import generate_chat_reply
@@ -21,6 +20,44 @@ import uvicorn
 app = FastAPI()
 
 PATH = '/api/v1/stream'
+
+# We use a thread local to store the asyncio lock, so that each thread
+# has its own lock.  This isn't strictly necessary, but it makes it
+# such that if we can support multiple worker threads in the future,
+# thus handling multiple requests in parallel.
+api_tls = threading.local()
+
+
+def _get_api_lock(tls) -> asyncio.Lock:
+    """
+    The streaming and blocking API implementations each run on their own
+    thread, and multiplex requests using asyncio.  If multiple outstanding
+    requests are received at once, we will try to acquire the shared lock
+    shared.generation_lock multiple times in succession in the same thread,
+    which will cause a deadlock.
+
+    To avoid this, we use this wrapper function to block on an asyncio
+    lock, and then try and grab the shared lock only while holding
+    the asyncio lock.
+    """
+    if not hasattr(tls, "asyncio_lock"):
+        tls.asyncio_lock = asyncio.Lock()
+
+    return tls.asyncio_lock
+
+
+def with_api_lock(func):
+    """
+    This decorator should be added to all streaming API methods which
+    require access to the shared.generation_lock.  It ensures that the
+    tls.asyncio_lock is acquired before the method is called, and
+    released afterwards.
+    """
+    @functools.wraps(func)
+    async def api_wrapper(*args, **kwargs):
+        async with _get_api_lock(api_tls):
+            return await func(*args, **kwargs)
+    return api_wrapper
 
 
 @with_api_lock
@@ -133,8 +170,7 @@ def _run_server(port: int, share: bool = False, tunnel_id=str):
 def start_server(port: int, share: bool = False, tunnel_id=str):
     Thread(target=_run_server, args=[
            port, share, tunnel_id], daemon=True).start()
-    Thread(target=_run_server_custom, args=[
-           5006, share, tunnel_id], daemon=True).start()
+    Thread(target=_run_server_custom, args=[5006], daemon=True).start()
 
 
 app = FastAPI()
@@ -143,48 +179,61 @@ app = FastAPI()
 @app.post("/")
 async def generate_stream(request: Request):
     async def generator(body: str):
-        body = json.loads(body)
+        async with _get_api_lock(api_tls):
+            body = json.loads(body)
 
-        prompt = body['inputs']
+            prompt = body['inputs']
 
-        body = body["parameters"]
-        body["prompt"] = prompt
-        if 'stop' in body:
-            body["stopping_strings"] = body.pop("stop")
-        if 'truncate' in body:
-            body["truncation_length"] = body.pop("truncate")
-        if 'typical_p' in body and body['typical_p'] is None:
-            body.pop("typical_p")
-        if 'typical' in body and body['typical'] is None:
-            body.pop("typical")
+            body = body["parameters"]
+            body["prompt"] = prompt
+            if 'stop' in body:
+                body["stopping_strings"] = body.pop("stop")
+            if 'truncate' in body:
+                body["truncation_length"] = body.pop("truncate")
+            if 'typical_p' in body and body['typical_p'] is None:
+                body.pop("typical_p")
+            if 'typical' in body and body['typical'] is None:
+                body.pop("typical")
 
-        async with websockets.connect("ws://localhost:5005/api/v1/stream", ping_interval=None) as websocket:
-            await websocket.send(json.dumps(body))
+            generate_params = build_parameters(body)
+            stopping_strings = generate_params.pop('stopping_strings')
+            generate_params['stream'] = True
 
-            while True:
-                incoming_data = await websocket.recv()
-                incoming_data = json.loads(incoming_data)
+            generator = generate_reply(
+                prompt, generate_params, stopping_strings=stopping_strings, is_chat=False)
 
-                match incoming_data['event']:
-                    case 'text_stream':
-                        text = incoming_data['text']
-                        yield json.dumps({
-                            "token": {
-                                "id": 0,
-                                "text": text,
-                                "logprob": 0,
-                                "special": False,
-                            },
-                            "generated_text": None,
-                            "details": None
-                        })
-                    case 'stream_end':
-                        return
+            # As we stream, only send the new bytes.
+            skip_index = 0
+            message_num = 0
+
+            for a in generator:
+                if await request.is_disconnected():
+                    return
+
+                to_send = a[skip_index:]
+                # partial unicode character, don't send it yet.
+                if to_send is None or chr(0xfffd) in to_send:
+                    continue
+
+                yield json.dumps({
+                    "token": {
+                        "id": 0,
+                        "text": to_send,
+                        "logprob": 0,
+                        "special": False,
+                    },
+                    "generated_text": None,
+                    "details": None
+                })
+
+                #await asyncio.sleep(0)
+                skip_index += len(to_send)
+                message_num += 1
 
     return EventSourceResponse(generator(await request.body()))
 
 
-def _run_server_custom(port: int, share: bool = False, tunnel_id=str):
+def _run_server_custom(port: int):
     address = '0.0.0.0' if shared.args.listen else '127.0.0.1'
 
     print(f'Starting custom streaming server at http://{address}:{port}/')
